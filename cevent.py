7024b4e54b051573bfdf1b0a2cdedb6185755e13
Matthew Wood <matt.wood@hp.com>
2011-03-25 23:47:49 -0600

    Made graphics work.

---
+++ cevent.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+"""Provides an pseudo-interface-class for pygame events."""
+
+
+class CEvent(object):
+    """Base class (interface) for handling pygame events."""
+
+    def __init__(self):
+        pass
+
+    def on_event(self, event):
+        pass
+
+    def on_inputFocus(self):
+        pass
+
+    def on_imputBlur(self):
+        pass
+
+    def on_key_down(self, event):
+        pass
+
+    def on_key_up(self, event):
+        pass
+
+    def on_mouse_focus(self):
+        pass
+
+    def on_mouse_blur(self):
+        pass
+
+    def on_mouse_move(self, event):
+        pass
+
+    def on_mouse_wheel(self, event):
+        pass
+
+    def on_lbutton_up(self, event):
+        pass
+
+    def on_lbutton_down(self, event):
+        pass
+
+    def on_rbutton_up(self, event):
+        pass
+
+    def on_rbutton_down(self, event):
+        pass
+
+    def on_mbutton_up(self, event):
+        pass
+
+    def on_mbutton_down(self, event):
+        pass
+
+    def on_minimize(self):
+        pass
+
+    def on_restore(self):
+        pass
+
+    def on_resize(self, event):
+        pass
+
+    def on_expose(self):
+        pass
+
+    def on_exit(self):
+        pass
+
+    def on_user(self, event):
+        pass
+
+    def on_joy_axis(self, event):
+        pass
+
+    def on_joybutton_up(self, event):
+        pass
+
+    def on_joybutton_down(self, event):
+        pass
+
+    def on_joy_hat(self, event):
+        pass
+
+    def on_joy_ball(self, event):
+        pass
+
+
+if __name__ == "__main__":
+    cevent = CEvent()
--- pipes.py
+++ pipes.py
@@ -2,73 +2,196 @@
 
 """This is a game for playing pipes. :-)"""
 
+import os
+import os.path
 import pygame
 import random
+import sys
+from collections import deque
 
 import graphlib
+import cevent
+
+
+PICS_DIR = 'pics'
+PIC_SIZE = 32
 
 
 class PipeSegment(object):
-    """A representation of a segment on the Pipes board."""
+    """A representation of a segfment on the Pipes board."""
+
+    screen = None
+
+    tiles = {}
+    for major in range(8):
+        for minor in range(16):
+            pic_file = os.path.join(PICS_DIR, '%03d_%03d.png' % (major, minor))
+            tiles[(major, minor)] = pygame.image.load(pic_file)
 
     initial_end_sets = {
-        'end-cap': frozenset(map(frozenset, [[0], [1], [2], [3]])),
-        'angle': frozenset(map(frozenset, [[0, 1], [1, 2], [2, 3], [3, 0]])),
-        'straight': frozenset(map(frozenset, [[0, 2], [1, 3]])),
-        'tee': frozenset(map(frozenset, [[0, 1, 2], [1, 2, 3],
-                                         [2, 3, 0], [3, 0, 1]])),
-        'cross': frozenset(map(frozenset, [[0, 1, 2, 3]])),
-    }
-
-    unset_chars = {
-        # End-Caps
-        frozenset([0]): u'\u2579', frozenset([1]): u'\u257a',
-        frozenset([2]): u'\u257b', frozenset([3]): u'\u2578',
-        # Angles
-        frozenset([0, 1]): u'\u2517', frozenset([1, 2]): u'\u250f',
-        frozenset([2, 3]): u'\u2513', frozenset([3, 0]): u'\u251b',
-        # Straights
-        frozenset([0, 2]): u'\u2503', frozenset([1, 3]): u'\u2501',
-        # T's
-        frozenset([0, 1, 2]): u'\u2523', frozenset([1, 2, 3]): u'\u2533',
-        frozenset([2, 3, 0]): u'\u252b', frozenset([3, 0, 1]): u'\u253b',
-        # Crosses
-        frozenset([0, 1, 2, 3]): u'\u254b',
+        'end-cap': tuple(map(frozenset, [[0],
+                                         [1],
+                                         [2],
+                                         [3]])),
+        'angle': tuple(map(frozenset, [[0, 1],
+                                       [1, 2],
+                                       [2, 3],
+                                       [3, 0]])),
+        'straight': tuple(map(frozenset, [[0, 2],
+                                          [1, 3]])),
+        'tee': tuple(map(frozenset, [[0, 1, 2],
+                                     [1, 2, 3],
+                                     [2, 3, 0],
+                                     [3, 0, 1]])),
+        'cross': tuple(map(frozenset, [[0, 1, 2, 3]])),
     }
 
     set_chars = {
         # End-Caps
-        frozenset([0]): u'\u2579', frozenset([1]): u'\u257a',
-        frozenset([2]): u'\u257b', frozenset([3]): u'\u2578',
+        frozenset([0]): u'\u2579',
+        frozenset([1]): u'\u257a',
+        frozenset([2]): u'\u257b',
+        frozenset([3]): u'\u2578',
         # Angles
-        frozenset([0, 1]): u'\u255a', frozenset([1, 2]): u'\u2554',
-        frozenset([2, 3]): u'\u2557', frozenset([3, 0]): u'\u255d',
+        frozenset([0, 1]): u'\u255a',
+        frozenset([1, 2]): u'\u2554',
+        frozenset([2, 3]): u'\u2557',
+        frozenset([3, 0]): u'\u255d',
         # Straights
-        frozenset([0, 2]): u'\u2551', frozenset([1, 3]): u'\u2550',
+        frozenset([0, 2]): u'\u2551',
+        frozenset([1, 3]): u'\u2550',
         # T's
-        frozenset([0, 1, 2]): u'\u2560', frozenset([1, 2, 3]): u'\u2566',
-        frozenset([2, 3, 0]): u'\u2563', frozenset([3, 0, 1]): u'\u2569',
+        frozenset([0, 1, 2]): u'\u2560',
+        frozenset([1, 2, 3]): u'\u2566',
+        frozenset([2, 3, 0]): u'\u2563',
+        frozenset([3, 0, 1]): u'\u2569',
         # Crosses
         frozenset([0, 1, 2, 3]): u'\u256c',
     }
 
-    def __init__(self, seg_type):
+    set_pic_nums = {
+        # No connection
+        frozenset([]): 0,
+        # End-Caps
+        frozenset([0]): 1,
+        frozenset([1]): 2,
+        frozenset([2]): 3,
+        frozenset([3]): 4,
+        # Angles
+        frozenset([0, 1]): 7,
+        frozenset([1, 2]): 8,
+        frozenset([2, 3]): 9,
+        frozenset([3, 0]): 10,
+        # Straights
+        frozenset([0, 2]): 5,
+        frozenset([1, 3]): 6,
+        # T's
+        frozenset([0, 1, 2]): 12,
+        frozenset([1, 2, 3]): 13,
+        frozenset([2, 3, 0]): 14,
+        frozenset([3, 0, 1]): 11,
+        # Crosses
+        frozenset([0, 1, 2, 3]): 15,
+    }
+
+    def __init__(self, seg_type, node):
         """
         Constructor for a pipe segment. seg_type can be:
         'end-cap', 'angle', 'straight', 'tee', or 'cross'.
         """
-        self.connections = set(self.initial_end_sets[seg_type])
+        self.connections = list(self.initial_end_sets[seg_type])
+        self.cursor = random.randrange(len(self.connections))
+        self.node = node
+        self.is_highlighted = False
+        self.is_attached = False
+
+    def rotate_right(self):
+        self.cursor += 1
+        if self.cursor >= len(self.connections):
+            self.cursor = 0
+
+    def rotate_left(self):
+        self.cursor -= 1
+        if self.cursor < 0:
+            self.cursor = len(self.connections) - 1
+
+    def attached_to_source(self):
+        return self.is_attached
 
     def is_set(self):
         """Returns True if the segment only has one possibility."""
         return 1 == len(self.connections)
 
+        return self._hightlighted
+
+    def get_major(self):
+        """f
+        Returns the tile's major number, which determines which tile-set to
+        use.
+        """
+        major = 0
+        if self.attached_to_source():
+            major += 1
+        if self.is_set():
+            major += 2
+        if self.is_highlighted:
+            major += 4
+        return major
+
+    def get_minor(self):
+        """
+        Returns the tile's minor number, which determines which tile shape to
+        use.
+        """
+        minor = self.set_pic_nums[self.connections[self.cursor]]
+        return minor
+
+    def on_render(self):
+        """Draws the square."""
+        major = self.get_major()
+        minor = self.get_minor()
+        tile = self.tiles[(major, minor)]
+        x, y = self.node
+        self.screen.blit(tile, (PIC_SIZE * x, PIC_SIZE * y))
+
     def __unicode__(self):
         """A unicode representation of a pipe segment."""
-        if self.is_set():
-            return self.set_chars[iter(self.connections).next()]
-        else:
-            return self.unset_chars[iter(self.connections).next()]
+        return self.set_chars[self.connections[self.cursor]]
+
+    def get_possible_connected_nodes(self):
+        """
+        Returns all possible node-positions that could connect to this node.
+        NOTE: This can include nodes that don't really connect, and nodes that
+              are off the edge of the board.
+        """
+        x, y = self.node
+        connected_nodes = deque()
+        for direction in self.connections[self.cursor]:
+            if direction == 0:
+                connected_nodes.append((x, y - 1))
+            elif direction == 1:
+                connected_nodes.append((x + 1, y))
+            elif direction == 2:
+                connected_nodes.append((x, y + 1))
+            elif direction == 3:
+                connected_nodes.append((x - 1, y))
+        return connected_nodes
+
+    def is_connected_to(self, pos):
+        sx, sy = self.node
+        rx, ry = pos
+        dx = sx - rx
+        dy = sy - ry
+
+        if dx == 0 and dy == 1:
+            return 0 in self.connections[self.cursor]
+        if dx == -1 and dy == 0:
+            return 1 in self.connections[self.cursor]
+        if dx == 0 and dy == -1:
+            return 2 in self.connections[self.cursor]
+        if dx == 1 and dy == 0:
+            return 3 in self.connections[self.cursor]
+
 
     def delete_connection(self, bad_option):
         """Delete any connection that contains bad_option."""
@@ -97,7 +220,7 @@ class PipeSegment(object):
         return bad_connections
 
 
-class PipesBoard(object):
+class PipesBoard(cevent.CEvent):
     """A class representing the game: Pipes!"""
 
     def __init__(self, size):
@@ -114,7 +237,19 @@ class PipesBoard(object):
         self.ys = range(y)
 
         self.board = {}
+        self.source = (0, 0)
+
+        self._is_running = False
+
+    def on_init(self):
+        """Creates the pygame board."""
         self.generate()
+        print unicode(self)
+        size = width, height = PIC_SIZE * len(self.xs), PIC_SIZE * len(self.ys)
+        pygame.init()
+        self.screen = pygame.display.set_mode(size)
+        PipeSegment.screen = self.screen
+        self._is_running = True
 
     def generate(self):
         """Generate a starting Pipes setup."""
@@ -129,16 +264,17 @@ class PipesBoard(object):
 
         graph = graph.min_span_tree()
 
-        for node, links in graph.nodes.iteritems():
+        for node, links in graph.nodes.items():
             if len(links) == 2:
                 node_a, node_b = links.keys()
                 if node_a[0] == node_b[0] or node_a[1] == node_b[1]:
-                    self.board[node] = PipeSegment('straight')
+                    self.board[node] = PipeSegment('straight', node)
                 else:
-                    self.board[node] = PipeSegment('angle')
+                    self.board[node] = PipeSegment('angle', node)
             else:
                 segment_types = {1: 'end-cap', 3: 'tee', 4: 'cross'}
-                self.board[node] = PipeSegment(segment_types[len(links)])
+                self.board[node] = PipeSegment(segment_types[len(links)], node)
+        self.source = random.choice(self.board.keys())
 
     def __unicode__(self):
         """A unicode grid of the pipes grid."""
@@ -150,11 +286,112 @@ class PipesBoard(object):
 
         return u''.join(ret_str)
 
+    #### Events ####
+    def on_event(self, event):
+        """Reacts to events."""
+        if event.type == pygame.QUIT:
+            self.on_exit()
+
+        elif event.type >= pygame.USEREVENT:
+            self.on_user(event)
+
+        elif event.type == pygame.VIDEOEXPOSE:
+            self.on_expose()
+
+        elif event.type == pygame.VIDEORESIZE:
+            self.on_resize(event)
+
+        elif event.type == pygame.KEYUP:
+            self.on_key_up(event)
+
+        elif event.type == pygame.KEYDOWN:
+            self.on_key_down(event)
+
+        elif event.type == pygame.MOUSEMOTION:
+            self.on_mouse_move(event)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                self.on_lbutton_up(event)
+            elif event.button == 2:
+                self.on_mbutton_up(event)
+            elif event.button == 3:
+                self.on_rbutton_up(event)
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                self.on_lbutton_down(event)
+            elif event.button == 2:
+                self.on_mbutton_down(event)
+            elif event.button == 3:
+                self.on_rbutton_down(event)
+
+    def on_exit(self):
+        self._is_running = False
+
+    def _get_node(self, pos):
+        x, y = pos
+        return (x / PIC_SIZE, y / PIC_SIZE)
+
+    def on_mouse_move(self, event):
+        active_node = self._get_node(event.pos)
+        for node, square in self.board.items():
+            if node == active_node:
+                square.is_highlighted = True
+            else:
+                square.is_highlighted = False
+
+    def on_lbutton_down(self, event):
+        node = self._get_node(event.pos)
+        self.board[node].rotate_right()
+
+    def on_rbutton_down(self, event):
+        node = self._get_node(event.pos)
+        self.board[node].rotate_left()
+
+    #### Loop ####
+    def on_loop(self):
+        """Modifies the environment based on signals from events."""
+        self.mark_attached()
+
+    def mark_attached(self):
+        for square in self.board.values():
+            square.is_attached = False
+
+        attached_nodes = deque()
+        attached_nodes.append(self.source)
+        while attached_nodes:
+            node = attached_nodes.pop()
+            square = self.board.get(node)
+            if square is None:
+                continue
+            if not square.is_attached:
+                for potential in square.get_possible_connected_nodes():
+                    p_square = self.board.get(potential)
+                    if p_square is None:
+                        continue
+                    if p_square.is_connected_to(node):
+                        attached_nodes.append(potential)
+            square.is_attached = True
+
+    #### Render ####
+    def on_render(self):
+        #self.screen.fill((1, 1, 1))
+        self.screen.fill((0, 0, 0))
+        for y in self.ys:
+            for x in self.xs:
+                self.board[(x, y)].on_render()
+        pygame.display.flip()
+
+    #### Cleanup ####
+    def on_cleanup(self):
+        """Clean up the pygame board."""
+        pygame.quit()
+
     def neighbor_nodes(self, position, directions):
         """Return the position and segment in the direction of position."""
         if directions is None:
             directions = set(0, 1, 2, 3)
-        pass
         # FIXME Here's where you start.
 
     def solve(self):
@@ -187,10 +424,21 @@ class PipesBoard(object):
             if position[1] == self.ys[-1]:
                 segment.delete_connection(2)
 
+    #### Main execution loop ####
+    def on_execute(self):
+        """Main Execution loop."""
+        self.on_init()
+
+        while self._is_running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+
+        self.on_cleanup()
+
 
 if __name__ == '__main__':
-    board = PipesBoard(4)
-    print unicode(board)
-    print
-    board.solve()
-    print unicode(board)
+
+    pipes = PipesBoard(16)
+    pipes.on_execute()
